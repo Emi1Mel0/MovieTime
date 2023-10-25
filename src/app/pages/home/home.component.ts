dc892a9a7578fff47a390d40ab57152fb436abbc
Emi1Mel0 <emilwagdy11@gmail.com>
2023-10-25 23:05:50 +0300

    getting data from service and putting in onInit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private _MovieApiServiceService: MovieApiServiceService) {}
 
+  bannerResult: any = [];
+  trendingMovieResult: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+    this.trendingData();
+  }
+
+  bannerData() {
+    this._MovieApiServiceService.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerResult#');
+      this.bannerResult = result.results;
+    });
+  }
+
+  trendingData(){
+    this._MovieApiServiceService.trendingMoviesApiData().subscribe((result)=>{
+      console.log(result,`trendingresults#`)
+      this.trendingMovieResult = result.results;
+    })
+  }
 }
