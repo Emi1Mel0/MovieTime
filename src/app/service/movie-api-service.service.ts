1a43584614c0e9b50b0daa09bb0334d1fb01b663
Emi1Mel0 <emilwagdy11@gmail.com>
2023-10-25 23:04:51 +0300

    getting data from the api link

--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieApiServiceService {
+  constructor(private _HttpClient: HttpClient) {}
 
-  constructor() { }
+  baseurl = 'https://api.themoviedb.org/3';
+  apikey = '1feaaea8618497e31f3989b8c5601e98';
+
+  bannerApiData(): Observable<any> {
+    return this._HttpClient.get(
+      `${this.baseurl}/trending/all/week?api_key=${this.apikey}`
+    );
+  }
+
+  trendingMoviesApiData(): Observable<any> {
+    return this._HttpClient.get(
+      `${this.baseurl}/trending/all/day?api_key=${this.apikey}`
+    );
+  }
 }
