9fb6e495e8ddc22a4872a45c5faf978baea24256
Emi1Mel0 <emilwagdy11@gmail.com>
2023-10-25 23:03:18 +0300

    importing two new things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
-
+import { HttpClientModule } from "@angular/common/http";
+import { MovieApiServiceService } from './service/movie-api-service.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,9 +19,10 @@ import { MovieDetailsComponent } from './pages/movie-details/movie-details.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MovieApiServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
